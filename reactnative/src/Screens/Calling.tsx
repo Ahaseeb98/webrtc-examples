56ebb916e63a128eabacf3236a3d082f19c5ab06
Ahaseeb98 <36041221+Ahaseeb98@users.noreply.github.com>
2025-02-05 22:49:12 +0500

    screens inprogress

--- reactnative/src/Screens/Calling.tsx
+++ reactnative/src/Screens/Calling.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useCallback} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 
-import {useRoute, RouteProp} from '@react-navigation/native';
+import {useRoute, RouteProp, useFocusEffect} from '@react-navigation/native';
 import {RootStackParamList} from '../Navigation';
+import IconButton from '../Components/IconButton';
+import {useWebRTC} from '../Providers/WebRCTProvider';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 type CallingScreenRouteProp = RouteProp<RootStackParamList, 'Calling'>;
 
 const Calling = () => {
   const params = useRoute<CallingScreenRouteProp>();
+  const otherUserId = params.params.otherId;
+  const {setOtherUserId, processCall, leave} = useWebRTC();
+  useFocusEffect(
+    useCallback(() => {
+      setOtherUserId(otherUserId);
+      processCall(otherUserId);
+    }, [otherUserId]),
+  );
   return (
     <View style={styles.container}>
       <Text style={styles.h1}>Calling...</Text>
       <View style={styles.myId}>
-        <Text style={styles.idText}>{params.params.otherId}</Text>
+        <Text style={styles.idText}>{otherUserId}</Text>
+      </View>
+      <View style={styles.content}>
+        <IconButton
+          backgroundColor="red"
+          onPress={() => leave()}
+          IconComponent={
+            <MaterialIcons name="call-end" size={22} color={'#ffffff'} />
+          }
+        />
       </View>
     </View>
   );
@@ -23,6 +44,11 @@ export default Calling;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
   },
   h1: {
     fontSize: 24,
--- reactnative/src/Screens/InCall.tsx
+++ reactnative/src/Screens/InCall.tsx
@@ -1,10 +1,115 @@
-import React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, {useCallback} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {RTCView} from 'react-native-webrtc';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {RouteProp, useFocusEffect, useRoute} from '@react-navigation/native';
+import inCallManager from 'react-native-incall-manager';
+import {useWebRTC} from './../Providers/WebRCTProvider';
+import IconButton from '../Components/IconButton';
+import {RootStackParamList} from '../Navigation';
+
+type CallingScreenRouteProp = RouteProp<RootStackParamList, 'Calling'>;
 
 const InCall = () => {
+  const params = useRoute<CallingScreenRouteProp>();
+  const otherUserId = params.params.otherId;
+  const {
+    localStream,
+    remoteStream,
+    leave,
+    switchCamera,
+    toggleCamera,
+    toggleMic,
+    localMicOn,
+    localWebcamOn,
+  } = useWebRTC();
+
+  useFocusEffect(
+    useCallback(() => {
+      inCallManager.start();
+      inCallManager.setKeepScreenOn(true);
+      inCallManager.setForceSpeakerphoneOn(true);
+      return () => {
+        inCallManager.stop();
+      };
+    }, []),
+  );
   return (
     <View style={styles.container}>
-      <Text style={styles.h1}>InCall</Text>
+      <View style={{flex: 1, zIndex: 10}}>
+        {remoteStream ? (
+          <RTCView
+            objectFit={'cover'}
+            style={styles.localStream}
+            streamURL={remoteStream.toURL()}
+          />
+        ) : (
+          <View style={styles.paused}>
+            <Text style={{color: '#fff'}}>Remote video is paused</Text>
+          </View>
+        )}
+        {localStream && localWebcamOn ? (
+          <RTCView
+            objectFit={'cover'}
+            style={styles.localStream}
+            streamURL={localStream.toURL()}
+          />
+        ) : (
+          <View style={styles.paused}>
+            <Text style={{color: '#fff'}}>Local video is paused</Text>
+          </View>
+        )}
+      </View>
+
+      {/* Controls */}
+      <View
+        style={{
+          padding: 12,
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          backgroundColor: '#222',
+        }}>
+        <IconButton
+          onPress={() => leave(false, otherUserId)}
+          IconComponent={
+            <MaterialIcons name="call-end" size={22} color="#FFF" />
+          }
+        />
+        <IconButton
+          style={{
+            backgroundColor: !localMicOn ? '#fff' : 'transparent',
+          }}
+          onPress={toggleMic}
+          IconComponent={
+            localMicOn ? (
+              <MaterialIcons name="mic" size={22} color="#FFF" />
+            ) : (
+              <MaterialIcons name="mic-off" size={22} color="#1D2939" />
+            )
+          }
+        />
+        <IconButton
+          style={{
+            backgroundColor: !localWebcamOn ? '#fff' : 'transparent',
+          }}
+          onPress={toggleCamera}
+          IconComponent={
+            localWebcamOn ? (
+              <MaterialIcons name="videocam" size={22} color="#FFF" />
+            ) : (
+              <MaterialIcons name="videocam-off" size={22} color="#1D2939" />
+            )
+          }
+        />
+        <IconButton
+          disabled={!localWebcamOn}
+          onPress={switchCamera}
+          IconComponent={
+            <MaterialIcons name="cameraswitch" size={22} color="#FFF" />
+          }
+        />
+      </View>
     </View>
   );
 };
@@ -20,5 +125,12 @@ const styles = StyleSheet.create({
   h1: {
     fontSize: 24,
     fontWeight: 'bold',
+  },
+  localStream: {flex: 1, backgroundColor: '#050A0E', marginTop: 8},
+  paused: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#222',
   },
 });
